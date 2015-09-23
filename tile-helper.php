7d51b32eb22984b4669552960b043d66208a2594
Ronny Wang <ronnywang@gmail.com>
2015-09-23 12:01:58 +0800

    加上 latLngDeltaPerMeter (某個座標點每公尺的經緯度解析度) 和 gridPoint(將點降解析度) 處理

--- tile-helper.php
+++ tile-helper.php
@@ -4,6 +4,41 @@ namespace TileHelper;
 
 class Helper
 {
+    /**
+     * latLngDeltaPerMeter 取得在 $lat, $lng 時，每公尺是多少經緯度
+     * 
+     * @param mixed $lat 
+     * @param mixed $lng 
+     * @access public
+     * @return void
+     */
+    public function latLngDeltaPerMeter($lat, $lng)
+    {
+        $earth_radius = 6378100;
+
+        $lat_delta = 180 / ($earth_radius * pi()); 
+        $lng_delta = 360 / (2 * pi() * $earth_radius * cos($lat * pi() / 180));
+
+        return array($lat_delta, $lng_delta);
+    }
+
+    /**
+     * gridPoint 把 ($lat, $lng) 降解析度到 ($lat_delta, $lng_delta)
+     * 
+     * @param mixed $lat 
+     * @param mixed $lng 
+     * @param mixed $lat_delta 
+     * @param mixed $lng_delta 
+     * @access public
+     * @return void
+     */
+    public function gridPoint($lat, $lng, $lat_delta, $lng_delta)
+    {
+        return array(
+            floor($lat / $lat_delta) * $lat_delta,
+            floor($lng / $lng_delta) * $lng_delta,
+        );
+    }
 }
 
 class WMTS
